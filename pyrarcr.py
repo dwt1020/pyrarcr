a83ef3b3bbb5c53df7f2d47878c07f3e0c6cbdd7
Alp Pirli <pirlialperen@yandex.com>
2014-01-20 10:35:08 +0200

    Added .zip support

--- pyrarcr.py
+++ pyrarcr.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python3
 
-#finds the password of a desired rar file using a brute-force algorithm
+#finds the password of a desired rar or zip file using a brute-force algorithm
 ##will fail to find the password if the password has a character that isnt in
-##the english alphabet or isnt a number
+##the english alphabet or isnt a number (you can change the char. list though)
 
 #importing needed modules
 import time,os,sys,shutil
 
-#checking if the user has unrar installed
-if not shutil.which("unrar"):
- print("ERROR: Unrar isn't installed.\nExiting...")
- sys.exit(-1)
+#checking if the user has unrar/p7zip installed
+for which in ["unrar","p7zip"]:
+ if not shutil.which(which):
+  print("ERROR:",which,"isn't installed.\nExiting...")
+  sys.exit(-1)
 
 #defining the function
 def rc(rf):
  alphabet="aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ1234567890"
  start=time.time()
  tryn=0
- if rf[-4:]==".rar":
-  for i in range(sys.maxsize):
-   a=[x for x in alphabet]
-   for j in range(i):
-    a=[x+i for x in alphabet for i in a]
-   for k in a:
+ for i in range(sys.maxsize):
+  a=[x for x in alphabet]
+  for j in range(i):
+   a=[x+i for x in alphabet for i in a]
+  for k in a: 
+   if rf[-4:]==".rar":
     print("Trying:",k)
     kf=os.popen("unrar t -y -p%s %s 2>&1|grep 'All OK'"%(k,rf))
     tryn+=1
@@ -34,9 +35,20 @@ def rc(rf):
       print("Exiting...")
       time.sleep(2)
       sys.exit(1)
-  print("Couldn't find the password.\nExiting...")
- else:
-  print("ERROR: File isnt a rar file.\nExiting...")
+   elif rf[-4:]==".zip":
+    print("Trying:",k)
+    kf=os.popen("7za t -p%s %s 2>&1|grep 'Everything is Ok'"%(k,rf))
+    tryn+=1
+    for rkf in kf.readlines():
+     if rkf=="Everything is Ok\n":
+      print("Found password:",repr(k))
+      print("Tried combination count:",tryn)
+      print("It took",round(time.time()-start,3),"seconds")
+      print("Exiting...")
+      time.sleep(2)
+      sys.exit(1)
+   else:
+    print("ERROR: File isnt a rar file.\nExiting...")
 
 #checking if the file exists/running the function
 if len(sys.argv)==2:
